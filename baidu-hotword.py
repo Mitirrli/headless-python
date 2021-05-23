b8972f3b75a803d33cb4796bd03351819ce22018
huanglei <huanglei@quanjingshuju.com>
2021-05-23 10:17:43 +0800

    百度热搜

--- baidu-hotword.py
+++ baidu-hotword.py
@@ -9,6 +9,6 @@ driver.get('https://www.baidu.com')
 try:
     WebDriverWait(driver, 20, 0.5).until(EC.presence_of_all_elements_located((By.CLASS_NAME,'s-hotsearch-title'))) #使用expected_conditions自带验证函数
     for title in driver.find_elements_by_css_selector('.title-content'):
-        print(title.find_element_by_css_selector('.title-content > .title-content-title').text)
+        print(title.find_element_by_css_selector('.title-content-title').text)
 finally:
     driver.close() # close the driver
