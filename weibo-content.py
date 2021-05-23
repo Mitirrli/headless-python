9e9ce030e87a8a0aa6f698cfb13d0e3d34e33e06
huanglei <huanglei@quanjingshuju.com>
2021-05-23 10:49:27 +0800

    新浪微博指定内容评论

---
+++ weibo-content.py
@@ -0,0 +1,14 @@
+# 爬取指定微博下的内容
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome()
+driver.get('https://weibo.com/2264713730/KgDR23sSX?ref=feedsdk&type=comment#_rnd1621736455489')
+try:
+    WebDriverWait(driver, 20, 0.5).until(EC.presence_of_all_elements_located((By.CLASS_NAME,'list_li'))) #使用expected_conditions自带验证函数
+    for title in driver.find_elements_by_css_selector('.list_con > .WB_text'):
+        print(title.text)
+finally:
+    driver.close() # close the driver
